897c0947b22a9037c2425f3a054dad4d0cba78ab
ahmad <ahmad.khaled2217@gmail.com>
2025-10-15 22:33:38 +0300

    Added basic calculator

--- Hello.py
+++ Hello.py
@@ -1,7 +1,16 @@
-print("Hello ")
-print("this is a test")
 
-x = 5
-y = 2
 
-print(f"x + y = {x + y}")
+
+z = str
+
+while z != 'q':
+    try:
+        x = int(input("Enter a number: "))
+        y = int(input("Enter another number: "))
+        print(f"x + y = {x + y}")
+        z = input("Press 'q' to quit or any other key to continue: ")
+        if z.lower() == 'q':
+            break
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
+        continue
